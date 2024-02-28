cd71580fa2ec960f2cb6d67a4a94e8d4f89220b4
Demid Greshnikov <demid.westhill@gmail.com>
2024-02-28 20:58:21 +0100

    add pet interface

---
+++ frontend/src/app/infrastructure/models/pet.model.ts
@@ -0,0 +1,10 @@
+export type Pet = {
+  id: string;
+  name: string;
+  age: number;
+  imageUrl: string;
+  description: string;
+  breed: string;
+  gender: 'male' | 'female';
+  category: 'cat' | 'dog';
+};
